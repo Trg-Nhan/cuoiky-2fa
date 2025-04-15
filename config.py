fb8900d1d89c21b58fb9ab687e9ff996de0449ed
Trg-Nhan <1050080148@sv.hcmunre.edu.vn>
2025-04-15 14:51:34 +0700

    🔗 Sử dụng DATABASE_URL kết nối tới MySQL nội bộ Railway

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-# config.py
-DEBUG = True
-SECRET_KEY = 'your_secret_key'  # Thay bằng key bảo mật của bạn
+import os
 
-# Kết nối MySQL qua pymysql.
-# Hãy thay thế username, password theo thông tin của bạn.
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost/2FA'
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
