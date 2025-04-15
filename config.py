a27859d1b3d76723b456810e1aa322e5513a4312
Trg-Nhan <1050080148@sv.hcmunre.edu.vn>
2025-04-15 14:56:02 +0700

    🔗 Sử dụng DATABASE_URL kết nối tới MySQL nội bộ Railway

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+DEBUG = os.getenv("DEBUG", "False") == "True"
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
