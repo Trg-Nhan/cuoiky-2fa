75c0b5701cf130864dc45d1fb6f1feebb6689d83
Trg-Nhan <1050080148@sv.hcmunre.edu.vn>
2025-04-19 11:17:02 +0700

    Cập nhật gửi config

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+# config.py
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-DEBUG = os.getenv("DEBUG", "False") == "True"
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+raw_db_url = os.getenv("DATABASE_URL", "")
+if raw_db_url.startswith("mysql://"):
+    raw_db_url = raw_db_url.replace("mysql://", "mysql+pymysql://")
+
+SQLALCHEMY_DATABASE_URI = raw_db_url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
